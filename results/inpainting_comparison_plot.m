9e2124df07ff844058693df448b4782260c7fbd2
Ondřej Mokrý <ondrej.mokry@mail.com>
2022-06-28 10:41:10 +0200

    Plotting scripts for the inpainting comparison

---
+++ results/inpainting_comparison_plot.m
@@ -0,0 +1,174 @@
+clear
+clc
+close all
+addpath('../utils')
+
+% settings
+filename  = 'inpainting_comparison_01.mat';
+bootstrap = false;
+intervals = false;
+
+% load data
+fprintf('Loading %s...\n',filename)
+load(filename)
+signals = signals(signums);
+
+% measures
+measures = {'SNR','PEMOQ','PEAQ'};
+ylabels  = {'SNR (dB)','PEMO-Q ODG','PEAQ ODG'};
+variants = {'end','max'};
+
+% prepare figure for SNR and ODG
+f_data = figure('visible','off','windowstate','maximized');
+T_data = tiledlayout(length(variants),length(measures),'tileindexing','columnmajor');
+colors = colororder();
+if length(methods) > 7
+    M = length(methods)-7;
+    colors = [colors; [zeros(M,2), (1:M)'/M]];
+end
+colororder(colors)
+
+% prepare figure for time
+f_time = figure('visible','off','windowstate','maximized');
+T_time = tiledlayout(length(variants),length(measures),'tileindexing','columnmajor');
+colororder(colors)
+
+for measure = 1:length(measures)
+    
+    %% reorganize the data for plotting
+    str = sprintf('measure: %s',measures{measure});
+    fprintf(repmat('=',1,length(str)))
+    fprintf('\n%s\n',str)
+    fprintf(repmat('=',1,length(str)))
+    fprintf('\n')
+    fprintf('Reorganizing the data...\n')
+    DATend = NaN(length(signums),length(glengths),length(methods));
+    DATmax = NaN(length(signums),length(glengths),length(methods));
+    TIMend = NaN(length(signums),length(glengths),length(methods));
+    TIMmax = NaN(length(signums),length(glengths),length(methods));
+    for i = 1:length(signums)
+        for j = 1:length(glengths)
+            for k = 1:length(methods)
+
+                % find the row
+                rows = strcmp(tables.(methods{k}).signal,signals{i});
+                rows = rows .* (tables.(methods{k}).gap == glengths(j));
+                row  = find(rows);
+                if isempty(row)
+                    continue
+                end
+
+                % read the value and save
+                DATend(i,j,k) = tables.(methods{k}).(measures{measure}){row}(end);
+                [DATmax(i,j,k), I] = max(tables.(methods{k}).(measures{measure}){row});
+                TIMend(i,j,k) = tables.(methods{k}).time(row);
+                TIMmax(i,j,k) = tables.(methods{k}).time(row) * I/length(tables.(methods{k}).(measures{measure}){row});
+            end
+        end
+    end
+
+    %% plotting means
+    for fig = 1:2
+        ax = gobjects(length(variants),length(measures));
+        for v = 1:length(variants)
+            if fig == 1
+                ax(v,measure) = nexttile(T_data);
+            else
+                ax(v,measure) = nexttile(T_time);
+            end
+            fprintf('data: %s, variant: %s\n',measures{measure},variants{v})
+            if strcmpi(variants{v},'end')
+                if fig == 2
+                    data = TIMend;
+                else
+                    data = DATend;
+                end
+            else
+                if fig == 2
+                    data = TIMmax;
+                else
+                    data = DATmax;
+                end
+            end
+
+            if intervals
+                if bootstrap
+                    % interval estimate using bootstrapping
+                    fprintf('   Computing the bootstraps...\n') %#ok<*UNRCH>
+                    [means, lowers, uppers] = bootstrap_est(data);
+                else
+                    fprintf('   Computing the stds...\n')
+                    mult   = tinv(0.975,length(signums)-1);
+                    means  = squeeze(mean(data,1,'omitnan'));
+                    stds   = squeeze(std(data,0,1,'omitnan'));
+                    lowers = means - mult*stds/sqrt(length(signums));
+                    uppers = means + mult*stds/sqrt(length(signums));
+                end
+            else
+                means = squeeze(mean(data,1,'omitnan'));
+            end
+
+            % plot
+            if intervals
+                fprintf('   Plotting...\n')
+            end
+            h = gobjects(length(methods),1);
+            for m = 1:length(methods)
+                if intervals
+                    h(m) = fillinterval(glengths,means(:,m),lowers(:,m),uppers(:,m),colors(m,:));
+                else
+                    h(m) = plot(glengths,means(:,m));
+                    hold on
+                end
+            end
+            legend(h,methods)
+            xlabel('gap length (ms)')
+            if fig == 2
+                set(gca,'YScale','log')
+                ylabel('elapsed time (s)')
+            else
+                ylabel(ylabels{measure})
+            end
+            if fig == 2
+                title(sprintf('elapsed time for %s, variant: %s',measures{measure},variants{v}))
+            else
+                title(sprintf('%s, variant: %s',measures{measure},variants{v}))
+            end
+            ylim('padded')
+        end
+        linkaxes(ax(:,measure),'y')
+    end
+    set(f_data,'visible','on')
+    set(f_time,'visible','on')
+
+    %% plotting individual signals
+    fprintf('Plotting individual results...\n')
+    for v = 1:length(variants)
+        f = figure('visible','off','windowstate','maximized');
+        colororder(colors)
+        t = tiledlayout(2,5,'tileindexing','columnmajor');
+        ax = gobjects(length(signums),1);
+
+        for s = 1:length(signums)
+            ax(s) = nexttile;
+            if strcmpi(variants{v},'end')
+                data = DATend;
+            else
+                data = DATmax;
+            end
+
+            % plot
+            plot(glengths,squeeze(data(s,:,:)))
+            legend(methodnames)
+            xlabel('gap length (ms)')
+            ylabel(ylabels{measure})
+            title(signals{s},'interpreter','none')
+
+        end
+
+        % link y axis and show
+        linkaxes(ax,'y')
+        title(t,sprintf('%s, variant: %s',measures{measure},variants{v}))
+        set(f,'visible','on')
+    end
+end
---
+++ results/inpainting_comparison_plot_nmfs.m
@@ -0,0 +1,206 @@
+clear
+close all
+addpath('../utils')
+
+% plot intervals?
+intervals = true;
+
+% compute bootstraps?
+bootstrap = false;
+
+%% load data
+fprintf('Loading data...\n')
+load('inpainting_comparison_01.mat')
+
+% consider only the NMF-based methods
+methods = methods([1:2, end]);
+methodnames = methodnames([1:2, end]);
+maxit = NMF.maxit;
+
+%% initialize the data arrays
+fprintf('Processing the data...\n')
+objective_abs = NaN(length(signums),length(glengths),maxit,length(methods));
+objective_rel = NaN(length(signums),length(glengths),maxit,length(methods));
+snr           = NaN(length(signums),length(glengths),maxit,length(methods));
+pemoq         = NaN(length(signums),length(glengths),maxit,length(methods));
+peaq          = NaN(length(signums),length(glengths),maxit,length(methods));
+relnorm       = NaN(length(signums),length(glengths),maxit,length(methods));
+
+%% fill the data arrays
+rowcounter = 0; % counter of the signal & gap length combinations
+sigcounter = 0; % counter of the signal
+for signum = signums
+    sigcounter = sigcounter + 1;
+    gapcounter = 0;
+    for glength = glengths
+        
+        rowcounter = rowcounter + 1;
+        gapcounter = gapcounter + 1;
+        
+        %% read data
+        for m = 1:length(methods)
+            L = length(tables.(methods{m}).SNR{rowcounter});
+        	snr(sigcounter,gapcounter,1:L,m)     = tables.(methods{m}).SNR{rowcounter};
+            pemoq(sigcounter,gapcounter,1:L,m)   = tables.(methods{m}).PEMOQ{rowcounter};
+            peaq(sigcounter,gapcounter,1:L,m)    = tables.(methods{m}).PEAQ{rowcounter}; 
+            relnorm(sigcounter,gapcounter,1:L,m) = tables.(methods{m}).relnorms{rowcounter};
+        end
+        
+    end
+end
+
+%% compute the bootstraps / stds
+if intervals
+    if bootstrap
+        fprintf('Computing the bootstraps (this may take some time)...\n') %#ok<*UNRCH>
+        addpath('../utils')
+        [means_1, lowers_1, uppers_1] = bootstrap_est(snr);
+        [means_2, lowers_2, uppers_2] = bootstrap_est(pemoq);
+        [means_3, lowers_3, uppers_3] = bootstrap_est(peaq);
+        [means_4, lowers_4, uppers_4] = bootstrap_est(log10(relnorm));
+    else
+        fprintf('Computing the stds...\n')
+        means_1  = squeeze(mean(snr,1,'omitnan'));
+        means_2  = squeeze(mean(pemoq,1,'omitnan'));
+        means_3  = squeeze(mean(peaq,1,'omitnan'));
+        means_4  = squeeze(mean(log10(relnorm),1,'omitnan'));
+        std_1    = squeeze(std(snr,0,1,'omitnan'));
+        std_2    = squeeze(std(pemoq,0,1,'omitnan'));
+        std_3    = squeeze(std(peaq,0,1,'omitnan'));
+        std_4    = squeeze(std(log10(relnorm),0,1,'omitnan'));
+        mult     = tinv(0.975,length(signums)-1);
+        lowers_1 = means_1 - mult*std_1/sqrt(length(signums));
+        uppers_1 = means_1 + mult*std_1/sqrt(length(signums));
+        lowers_2 = means_2 - mult*std_2/sqrt(length(signums));
+        uppers_2 = means_2 + mult*std_2/sqrt(length(signums));
+        lowers_3 = means_3 - mult*std_3/sqrt(length(signums));
+        uppers_3 = means_3 + mult*std_3/sqrt(length(signums));
+        lowers_4 = means_4 - mult*std_4/sqrt(length(signums));
+        uppers_4 = means_4 + mult*std_4/sqrt(length(signums));
+    end
+else
+    means_1 = squeeze(mean(snr,1,'omitnan'));
+    means_3 = squeeze(mean(pemoq,1,'omitnan'));
+    means_3 = squeeze(mean(peaq,1,'omitnan'));
+    means_4 = squeeze(mean(log10(relnorm),1,'omitnan'));
+end
+
+%% plot
+if intervals
+    if bootstrap
+        fprintf('Plotting the bootstraps...\n')
+    else
+        fprintf('Plotting the means + stds...\n')
+    end
+else
+    fprintf('Plotting...\n')
+end
+colors = colororder;
+figure('visible','off')
+T  = tiledlayout(length(glengths),4,'tileindexing','rowmajor');
+h  = gobjects(length(methods),1);
+ax = gobjects(4,length(glengths));
+for gapcounter = 1:length(glengths)
+    
+    % SNR
+    ax(1,gapcounter) = nexttile;
+    for m = 1:length(methods)
+        if intervals
+            h(m) = fillinterval(...
+                1:maxit,...
+                means_1(gapcounter,:,m),...
+                lowers_1(gapcounter,:,m),...
+                uppers_1(gapcounter,:,m),...
+                colors(m,:));
+            hold on
+        else
+            h(m) = plot(1:maxit,means_1(gapcounter,:,m),'color',colors(m,:));
+            hold on
+        end
+    end
+    legend(h,methodnames,'location','southeast')
+    xlabel('iteration')
+    ylabel('SNR [dB]')
+    title(sprintf('gap length %d ms',glengths(gapcounter)))
+    xlim([0 50])
+    
+    % PEMO-Q
+    ax(2,gapcounter) = nexttile;
+    for m = 1:length(methods)
+        if intervals
+            h(m) = fillinterval(...
+                1:maxit,...
+                means_2(gapcounter,:,m),...
+                lowers_2(gapcounter,:,m),...
+                uppers_2(gapcounter,:,m),...
+                colors(m,:));
+            hold on
+        else
+            h(m) = plot(1:maxit,means_2(gapcounter,:,m),'color',colors(m,:));
+            hold on
+        end
+    end
+    legend(h,methodnames,'location','southeast')
+    xlabel('iteration')
+    ylabel('PEMO-Q ODG')
+    title(sprintf('gap length %d ms',glengths(gapcounter)))
+    xlim([0 50])
+    
+    % PEAQ
+    ax(3,gapcounter) = nexttile;
+    for m = 1:length(methods)
+        if intervals
+            h(m) = fillinterval(...
+                1:maxit,...
+                means_3(gapcounter,:,m),...
+                lowers_3(gapcounter,:,m),...
+                uppers_3(gapcounter,:,m),...
+                colors(m,:));
+            hold on
+        else
+            h(m) = plot(1:maxit,means_3(gapcounter,:,m),'color',colors(m,:));
+            hold on
+        end
+    end
+    legend(h,methodnames,'location','southeast')
+    xlabel('iteration')
+    ylabel('PEAQ ODG')
+    title(sprintf('gap length %d ms',glengths(gapcounter)))
+    xlim([0 50])
+    
+    % relative change of the solution
+    ax(4,gapcounter) = nexttile;
+    for m = 1:length(methods)
+        if intervals
+            h(m) = fillinterval(...
+                2:maxit,...
+                10.^means_4(gapcounter,2:maxit,m),...
+                10.^lowers_4(gapcounter,2:maxit,m),...
+                10.^uppers_4(gapcounter,2:maxit,m),...
+                colors(m,:));
+                hold on
+        else
+            h(m) = plot(1:maxit,10.^means_4(gapcounter,:,m),'color',colors(m,:));
+            hold on
+        end
+    end
+    legend(h,methodnames)
+    xlabel('iteration')
+    ylabel('relative solution change')
+    set(gca,'YScale','log')
+    title(sprintf('gap length %d ms',glengths(gapcounter)))
+    xlim([0 50])
+end
+if intervals
+    if bootstrap
+        title(T,sprintf('bootstrap estimate of the mean performance from %d signals',length(signums)))
+    else
+        title(T,sprintf('normal estimate of the performance from %d signals',length(signums)))
+    end
+else
+    title(T,sprintf('mean performance from %d signals',length(signums)))
+end
+for i = 1:4
+    linkaxes(ax(i,:),'y')
+end
+set(gcf,'visible','on','windowstate','maximized')
