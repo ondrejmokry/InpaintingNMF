e92d5cce6cbe1054259296db2e8784cdb52b938a
Ondřej Mokrý <ondrej.mokry@mail.com>
2022-06-28 07:47:50 +0200

    Inpainting demo

---
+++ inpainting_demo.m
@@ -0,0 +1,93 @@
+clear
+clc
+close all
+rng(0)
+addpath('utils')
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                         %
+%                               the inputs                                %
+%                                                                         %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% create the signal and mask
+% signal
+[y, fs] = audioread('signals/mamavatu.wav');
+y  = resample(y,16e3,fs);
+fs = 16e3;
+L  = length(y);
+
+% mask
+mask = rand(L,1) > 0.6;
+
+%% settings
+% algorithm
+maxit = 100; % (50 in declipping survey)
+nmfit = 1;   % (1 in declipping survey)
+epsilon = 1e-4;
+
+% segmentation
+M = 1024; % segment length (2048 in declipping survey)
+a = 512;  % segment shift  (1024 in declipping survey)
+
+% model
+F = 2*M;  % number of frequency channels (M in declipping survey)
+K = 10;   % number of components for NMF (20 in declipping survey)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                         %
+%                              the algorithm                              %
+%                                                                         %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+methods = {'AM','EMtf',5,'EMtf','EMt'};
+methodnames = {...
+    'AM (F = M)',...
+    'EM-tf (F = M)',...
+    'AM-to-EM-tf (F = M)',...
+    'EM-tf (F = 2M)',...
+    'EM-t (F = 2M)'};
+
+results.signals    = cell(length(methods),1);
+results.Ws         = cell(length(methods),1);
+results.Hs         = cell(length(methods),1);
+results.SNRs       = cell(length(methods),1);
+results.objectives = cell(length(methods),1);
+
+for m = 1:length(methods)
+    
+    tic
+    if m <= 3 % the case with F = M: AM, EM-tf (equivalent with EM-t), switching
+        [restored, W, H, ~, objectives] = ainmf(methods{m},y,mask,K,maxit,...
+            'M',M,'a',a,'F',M,'saveall',true,'verbose',true,'nmfit',nmfit,...
+            'drawing',true,...
+            'epsilon',epsilon);
+    else % the case with F = 2*M: EM-tf, EM-t
+        [restored, W, H, ~, objectives] = ainmf(methods{m},y,mask,K,maxit,...
+            'M',M,'a',a,'F',F,'saveall',true,'verbose',true,'nmfit',nmfit,...
+            'drawing',true,...
+            'epsilon',epsilon);
+    end
+    t = toc;
+ 
+    % compute SNRs
+    SNRs = NaN(size(restored,2),1);
+    for i = 1:size(restored,2)
+        SNRs(i) = snr(y(~mask),y(~mask)-restored(~mask,i));
+    end
+    
+    fprintf('     Elapsed time is %.1f seconds.\n',t)
+    fprintf('     SNR is %.2f dB.\n',SNRs(end))
+    
+    % save
+    results.signals{m}    = restored;
+    results.Ws{m}         = W;
+    results.Hs{m}         = H;
+    results.SNRs{m}       = SNRs;
+    results.objectives{m} = objectives;
+    
+end
+
+%% save the data
+dt = datetime(clock);
+save(fname,'a','dt','epsilon','F','K','M','mask','maxit','methodnames','methods','nmfit','results','y')
---
+++ results/inpainting_demo_plot.m
@@ -0,0 +1,96 @@
+clear
+close all
+
+% load data
+load('inpainting_demo_01.mat')
+
+% format of the first graph
+stacked = true;
+%#ok<*UNRCH>
+
+%% time-domain solutions
+figure
+for method = 1:length(methods)
+    plot(results.signals{method}(:,end))
+    hold on
+end
+plot(y)
+xlim([1 length(y)])
+legend([methodnames,'original'])
+set(gcf,'windowstate','maximized')
+
+%% objective functions
+figure
+tl = tiledlayout(2,3);
+data = NaN(maxit,length(methods));
+for method = 1:length(methods)
+    data(1:length(results.objectives{method}),method) = results.objectives{method};
+end
+if stacked
+    s = tiledlayout(tl,length(methods),1,'tilespacing','tight','padding','tight');
+    s.Layout.TileSpan = [2 1];
+    C = colororder;
+    for method = 1:length(methods)
+        nexttile(s)
+        plot(data(:,method),'color',C(method,:))
+        legend(methodnames{method})
+        ylim('padded')
+        if method == 3
+            ylabel('-log likelihood')
+        end
+    end
+else
+    nexttile(tl,[2 1])
+    plot(data)
+    ylabel('-log likelihood (may differ between methods)')
+    ylim('padded')
+    legend(methodnames)
+end
+xlabel('iteration')
+
+%% SNRs
+nexttile(tl,[2 1])
+for method = 1:length(methods)
+    plot(results.SNRs{method})
+    hold on
+end
+xlabel('iteration')
+ylabel('SNR (dB)')
+ylim('padded')
+legend(methodnames,'location','southeast')
+
+%% relative change of objective
+nexttile(tl)
+for method = 1:length(methods)
+    data = Inf(length(results.objectives{method}),1);
+    for i = 2:length(results.objectives{method})
+        data(i) = abs(results.objectives{method}(i)-results.objectives{method}(i-1))...
+            /abs(results.objectives{method}(i-1));
+    end
+    semilogy(data)
+    hold on
+end
+hold off
+xlabel('iteration')
+ylabel('relative change of the objective')
+ylim('padded')
+legend(methodnames)
+
+%% relative norms
+nexttile(tl)
+for method = 1:length(methods)
+    data = Inf(size(results.signals{method},2),1);
+    for i = 2:size(results.signals{method},2)
+        data(i) = norm(results.signals{method}(:,i)-results.signals{method}(:,i-1))...
+            /norm(results.signals{method}(:,i-1));
+    end
+    semilogy(data)
+    hold on
+end
+hold off
+xlabel('iteration')
+ylabel('relative change of the solution')
+ylim('padded')
+legend(methodnames)
+
+set(gcf,'windowstate','maximized')
